361c793ff87d7078abc6e60b401988ec917e98d8
PX988 <psquecco@gmail.com>
2016-02-08 09:13:04 +0100

    rilocazione esercizi array

---
+++ ex_lesson_02/src/esercizio_7.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int mult(int a, int b)
+{
+	int m = 0;
+	for (int i = 0; i < a; i++)
+		m += b;
+
+	return m;
+}
+
+int powr(int x, int y)
+{
+	if (y == 0)
+		return 1;
+	if (y == 1)
+		return x;
+
+	return mult(x, powr(x, y-1));
+}
+
+void main()
+{
+	int x = 3, y = 4;
+
+	int p = powr(x, y);
+	printf("%d\n", p);
+}
